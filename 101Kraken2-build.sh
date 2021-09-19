4435c35f436c4a833a9090fc8f0980c8b921b784
Hidemasa Bono <bonohu@gmail.com>
2021-09-19 12:57:51 +0900

    Do not use docker

--- 101Kraken2-build.sh
+++ 101Kraken2-build.sh
@@ -7,13 +7,13 @@ DBREFFASTA=$1
 THREADS=14
 
 # 2. Downloading required file(taxonomy and so on: `names.dmp` and `nodes.dmp`)
-#kraken2-build --download-taxonomy --db $DBNAME
-docker run -it -v `pwd`:/kk2 staphb/kraken2 kraken2-build --download-taxonomy --db /kk2/db
+kraken2-build --download-taxonomy --db $DBNAME
+#docker run -it -v `pwd`:/kk2 staphb/kraken2 kraken2-build --download-taxonomy --db /kk2/db
 
 # 3. Installing reference library  
-#kraken2-build --add-to-library 16S_kraken2.fasta --threads {n} --db $DBNAME
-docker run -it -v `pwd`:/kk2 staphb/kraken2 kraken2-build --add-to-library /kk2/$DBREFFASTA --threads $THREADS --db /kk2/db
+kraken2-build --add-to-library 16S_kraken2.fasta --threads {n} --db $DBNAME
+#docker run -it -v `pwd`:/kk2 staphb/kraken2 kraken2-build --add-to-library /kk2/$DBREFFASTA --threads $THREADS --db /kk2/db
 
 # 4. Building database
-#kraken2-build --build --threads $THREADS --db $DBNAME
-docker run -it -v `pwd`:/kk2 staphb/kraken2 kraken2-build --build --threads $THREADS --db /kk2/db
+kraken2-build --build --threads $THREADS --db $DBNAME
+#docker run -it -v `pwd`:/kk2 staphb/kraken2 kraken2-build --build --threads $THREADS --db /kk2/db
