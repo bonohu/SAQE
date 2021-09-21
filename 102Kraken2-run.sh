cc83a0e18d23b767878e55164167a64947c381a6
Hidemasa Bono <bonohu@gmail.com>
2021-09-21 13:54:46 +0900

    output file location changed

--- 102Kraken2-run.sh
+++ 102Kraken2-run.sh
@@ -2,10 +2,10 @@
 
 # 1. Set working directory 
 FQ=$1
-REPORTF="$FQ.rep.txt"
-#THREADS=14
+REPORTF=`basename $FQ`.out.txt
+THREADS=14
+DBNAME="/Users/bono/kraken2"
 
 # 1. Classification & output
 kraken2 --db $DBNAME --gzip-compressed $FQ --threads $THREADS --report $REPORTF --output -
 #docker run -it --env KRAKEN2_DB_PATH=`pwd`/db -v `pwd`:/kk2 staphb/kraken2 kraken2 --db /kk2/db --gzip-compressed /kk2/$FQ --report /kk2/$REPORTF --output - 
- ```
