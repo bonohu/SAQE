1af28263520ad12552a81bd7788c6804d70684a0
Hidemasa Bono <bonohu@gmail.com>
2022-01-06 15:52:44 +0900

    Add GetRefProts

---
+++ 12GetRefProts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# GetRefs: download reference protein datasets
+
+# human
+curl -O https://ftp.ensembl.org/pub/current_fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz
+# mouse
+curl -O https://ftp.ensembl.org/pub/current_fasta/mus_musculus/pep/Mus_musculus.GRCm39.pep.all.fa.gz
+# fly
+curl -O https://ftp.ensembl.org/pub/current_fasta/drosophila_melanogaster/pep/Drosophila_melanogaster.BDGP6.32.pep.all.fa.gz
+# worm
+curl -O https://ftp.ensembl.org/pub/current_fasta/caenorhabditis_elegans/pep/Caenorhabditis_elegans.WBcel235.pep.all.fa.gz
