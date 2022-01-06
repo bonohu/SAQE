cf38ab24c7667f51c166ed1e27084727e10b7357
Hidemasa Bono <bonohu@gmail.com>
2022-01-06 09:27:47 +0900

    Add comments

--- 11TransDecoder.sh
+++ 11TransDecoder.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # TransDecoder: predict protein coding regions from transcripts
 
+# filename for Trinity output (default: Trinity.fasta)
 transcript=Trinity.fasta
 
+# Run transdecoder 
 TransDecoder.LogOrfs $transcript
 TransDecoder.Predict $transcript
